5d0e9dc3f723cddab97165daa79e5be59ebe5f2e
steven <carzysteven@gmail.com>
2018-06-25 16:42:37 +0800

    Fix 採用 uri 方式執行。

--- application/tests/controllers/Restful_test.php
+++ application/tests/controllers/Restful_test.php
@@ -12,21 +12,14 @@ class Restful_test extends TestCase {
   }
 
   public function test_index_get(){
-    // $output = $this->request('GET', 'http://127.0.0.1/index.php/restful/index');
-    // $output = $this->request('GET', 'restful/index');
-    // $this->get('restful/index');
-    $res = $this->http->request('GET', 'https://127.0.0.1/index.php/restful/index');
+    $uri = new Uri('/restful/index');
+    $res = $this->http->request('GET', $rui);
+    // $res = $this->http->request('GET', 'http://127.0.0.1/index.php/restful/index');
     $response = json_decode($res->getBody());
-    // $response = json_decode($this->response->getBody(), true);
-    // echo "<br>";
-    // print_r($response);
-    // var_dump($output);
-    // $response = json_decode($res->getBody());
-    // print_r($output);
-    // $this->assertObjectHasAttribute('id', $response);
-    // $this->assertObjectHasAttribute('method', $response);
-    // $this->assertObjectHasAttribute('createdAt', $response);
-    // $this->assertResponseCode(200);
+    $this->assertObjectHasAttribute('id', $response);
+    $this->assertObjectHasAttribute('method', $response);
+    $this->assertObjectHasAttribute('createdAt', $response);
+    $this->assertResponseCode(200);
   }
 
   public function test_index_post(){
