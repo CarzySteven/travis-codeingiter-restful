aaaee7ba316a0b6094c42f2bafe6005166fa5954
steven <carzysteven@gmail.com>
2018-06-25 14:37:00 +0800

    Fix 改用guzzie

--- application/tests/controllers/Restful_test.php
+++ application/tests/controllers/Restful_test.php
@@ -4,14 +4,23 @@
 
 class Restful_test extends TestCase {
   private $ci;
+  protected $http = null;
+
   function setUp() {
       parent::setUp();
+      $this->http = new \GuzzleHttp\Client();
   }
 
   public function test_index_get(){
     // $output = $this->request('GET', 'http://127.0.0.1/index.php/restful/index');
-    $output = $this->request('GET', 'restful/index');
-    var_dump($output);
+    // $output = $this->request('GET', 'restful/index');
+    // $this->get('restful/index');
+    $res = $this->http->request('GET', 'http://127.0.0.1/index.php/restful/index');
+    $response = json_decode($res->getBody());
+    // $response = json_decode($this->response->getBody(), true);
+    // echo "<br>";
+    // print_r($response);
+    // var_dump($output);
     // $response = json_decode($res->getBody());
     // print_r($output);
     // $this->assertObjectHasAttribute('id', $response);
